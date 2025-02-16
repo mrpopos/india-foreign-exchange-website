bec00aa477494190cc000e7c02444dc7c3416359
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-02-16 22:38:29 +0800

    quick start

--- src/views/quick-start/quickStart.vue
+++ src/views/quick-start/quickStart.vue
@@ -33,7 +33,7 @@ watchEffect(() => {
               How to trade<br />
               on financial markets
             </h1>
-            <div class="desc">
+            <div class="desc text-sm text-gray-700">
               <p class="text-white text-base md:text-xl">With Pocket Option</p>
             </div>
             <div>
@@ -47,95 +47,124 @@ watchEffect(() => {
       <!-- Get started in a few easy steps -->
       <section class="get-started w-full py-20 bg-gradient-to-br from-blue-200 to-slate-200">
         <div class="w-full px-6 mx-auto mesm:px-12 memd:px-16 melg:max-w-[1200px] melg:px-24">
-          <h2 class="title">Get started in a few easy steps</h2>
-          <div class="step-imgtxt">
-            <div class="step-item">
-              <div class="step-txt">
-                <div class="txt-title">
-                  <span>1</span>
-                  <span>Registration</span>
-                </div>
-                <div class="desc">
-                  Create a free trading account by using your email address or simply authorize via
-                  Facebook or Google accounts.
-                </div>
-              </div>
-              <div class="step-img">
-                <img src="@/assets/images/step1.webp" alt="step-1" />
-              </div>
-            </div>
-            <div class="step-item">
-              <div class="step-img">
-                <img src="@/assets/images/step2.webp" alt="step-1" />
+          <h2 class="title text-gray-800 px-6 py-4 text-2xl font-semibold mb-8">
+            Get started in a few easy steps
+          </h2>
+          <div class="step-imgtxt grid grid-cols-1 gap-8 memd:gap-16">
+            <div class="step-item flex flex-col justify-start items-center gap-4 memd:flex-row">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute w-full" src="@/assets/images/step1.webp" alt="step-1" />
+                <img class="w-full" src="@/assets/images/step1-1.webp" alt="step-1" />
               </div>
               <div class="step-txt">
                 <div class="txt-title">
-                  <span>2</span>
-                  <span>Registration</span>
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >1</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Registration</span>
                 </div>
-                <div class="desc">
+                <div class="desc text-sm text-gray-700">
                   Create a free trading account by using your email address or simply authorize via
                   Facebook or Google accounts.
                 </div>
               </div>
             </div>
-            <div class="step-item">
-              <div class="step-txt">
-                <div class="txt-title">
-                  <span>3</span>
-                  <span>Registration</span>
-                </div>
-                <div class="desc">
-                  Create a free trading account by using your email address or simply authorize via
-                  Facebook or Google accounts.
-                </div>
-              </div>
-              <div class="step-img">
-                <img src="@/assets/images/step3.webp" alt="step-1" />
-              </div>
-            </div>
-            <div class="step-item">
-              <div class="step-img">
-                <img src="@/assets/images/step4.webp" alt="step-1" />
+            <div class="step-item flex flex-col justify-start items-center gap-4 memd:flex-row-reverse">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute" src="@/assets/images/step2.webp" alt="step-1" />
+                <img src="@/assets/images/step2-1.webp" alt="step-1" />
               </div>
               <div class="step-txt">
                 <div class="txt-title">
-                  <span>4</span>
-                  <span>Registration</span>
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >2</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Verification</span>
                 </div>
-                <div class="desc">
-                  Create a free trading account by using your email address or simply authorize via
-                  Facebook or Google accounts.
+                <div class="desc text-sm text-gray-700">
+                  Make your account personalized. Enter your personal information in the profile and
+                  upload both ID document and address documents.
                 </div>
               </div>
             </div>
-            <div class="step-item">
+            <div class="step-item flex flex-col justify-start items-center gap-4  memd:flex-row">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute" src="@/assets/images/step3.webp" alt="step-1" />
+                <img src="@/assets/images/step3-1.webp" alt="step-1" />
+              </div>
               <div class="step-txt">
                 <div class="txt-title">
-                  <span>5</span>
-                  <span>Registration</span>
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >3</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Deposit</span>
                 </div>
-                <div class="desc">
-                  Create a free trading account by using your email address or simply authorize via
-                  Facebook or Google accounts.
+                <div class="desc text-sm text-gray-700">
+                  Add funds to your trading account balance by using the most comfortable deposit
+                  method. Processing time depends on the chosen option.
                 </div>
-              </div>
-              <div class="step-img">
-                <img src="@/assets/images/step5.webp" alt="step-1" />
               </div>
             </div>
-            <div class="step-item">
-              <div class="step-img">
-                <img src="@/assets/images/step6.webp" alt="step-1" />
+            <div class="step-item flex flex-col justify-start items-center gap-4  memd:flex-row-reverse">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute" src="@/assets/images/step4.webp" alt="step-1" />
+                <img src="@/assets/images/step4-1.webp" alt="step-1" />
               </div>
               <div class="step-txt">
                 <div class="txt-title">
-                  <span>6</span>
-                  <span>Registration</span>
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >4</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Trading</span>
                 </div>
-                <div class="desc">
-                  Create a free trading account by using your email address or simply authorize via
-                  Facebook or Google accounts.
+                <div class="desc text-sm text-gray-700">
+                  Trading on Pocket Option is easy as 123. Choose a trading asset, set up the
+                  preferred chart layout and enable indicators for better market analysis. Set the
+                  trade amount, purchase time and place either a price decrease or increase order.
+                </div>
+              </div>
+            </div>
+            <div class="step-item flex flex-col justify-start items-center gap-4 memd:flex-row">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute w-full" src="@/assets/images/step5.webp" alt="step-1" />
+                <img src="@/assets/images/step5-1.webp" alt="step-1" />
+              </div>
+              <div class="step-txt">
+                <div class="txt-title">
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >5</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Profit</span>
+                </div>
+                <div class="desc text-sm text-gray-700">
+                  Each correct forecast results in a profitable trade order. The order amount plus
+                  the generated profit are automatically added to your account balance. Manage your
+                  income properly, invest further or withdraw profit if necessary.
+                </div>
+              </div>
+            </div>
+            <div class="step-item flex flex-col justify-start items-center gap-4 memd:flex-row-reverse">
+              <div class="step-img relative w-[210px] mesm:w-[240px] memd:flex-none">
+                <img class="absolute" src="@/assets/images/step6.webp" alt="step-1" />
+                <img src="@/assets/images/step6-1.webp" alt="step-1" />
+              </div>
+              <div class="step-txt">
+                <div class="txt-title">
+                  <span
+                    class="num pr-2 text-6xl font-semibold bg-gradient-to-r from-blue-600 to-green-800 text-transparent bg-clip-text"
+                    >6</span
+                  >
+                  <span class="num-title text-gray-800 text-xl font-semibold">Withdrawal</span>
+                </div>
+                <div class="desc text-sm text-gray-700">
+                  You can withdraw your trading account balance at any time without any restrictions
+                  on the amount. Place a withdrawal request via one of the methods previously used
+                  for depositing and wait for it to be processed and sent.
                 </div>
               </div>
             </div>
